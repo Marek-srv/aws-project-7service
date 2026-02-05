120751c1570208620f9c3f8617ed6d4d000e7c9f
Marek_SRV <sriramvishal2001@gmail.com>
2026-02-05 12:55:13 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_CxwfmVV6o', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7hqkvhir76sue3fbhe259k09cf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_xI9mkBgXU', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '40fb6r00mplcqjmour0s3s6fhf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://ah93yq8y22.execute-api.us-east-1.amazonaws.com/Dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
